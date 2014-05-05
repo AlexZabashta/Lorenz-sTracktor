28e5ca58d2cde2c1846d300afe94e977a0fc2869
U-PC\Admin <Admin@PC.(none)>
2014-05-06 00:36:40 +0400

    add delta r and input

--- MainFrame.java
+++ MainFrame.java
@@ -1,43 +1,92 @@
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.WindowConstants;
 
 public class MainFrame {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 
 		Random rnd = new Random();
 
-		List<Plot3D> testPlot = new ArrayList<Plot3D>();
+		final List<Plot3D> testPlot = new ArrayList<Plot3D>();
 
-		double[][][] points = new double[3][3][30000];
+		Scanner sc = new Scanner(new File("input.txt"));
+
+		sc.useLocale(Locale.ENGLISH);
+		final int m = sc.nextInt(), n = sc.nextInt();
+		final double dt = sc.nextDouble(), r = sc.nextDouble();
+
+		final int[] met = new int[m];
+
+		for (int i = 0; i < m; i++) {
+			met[i] = sc.nextInt();
+			double sx = sc.nextDouble(), sy = sc.nextDouble(), sz = sc.nextDouble();
+			int red = sc.nextInt(), green = sc.nextInt(), blue = sc.nextInt();
+			int color = (red << 16) | (green << 8) | blue;
+			testPlot.add(new Plot3D(LorenzSystemSolver.solove(met[i], r, sx, sy, sz, dt, n), color));
+		}
+
+		sc.close();
+
+		int nc = 30000;
+		double lc = 100.0;
+
+		double[][] cord = new double[3][nc * 3];
 		for (int d = 0; d < 3; d++) {
-			for (int i = 0; i < 30000; i++) {
-				points[d][d][i] = i / 100.0;
+			for (int i = 0; i < nc; i++) {
+				cord[d][nc * d + i] = i * lc / nc;
 			}
 		}
 
-		for (int d = 0; d < 3; d++) {
-			testPlot.add(new Plot3D(points[d][0], points[d][1], points[d][2], 0xFFFFFFFF));
-		}
+		testPlot.add(new Plot3D(cord, 0xFFFFFF));
 
-		int n = 10000;
-		double r = rnd.nextDouble() * 100;
-
-		double sx = rnd.nextDouble() * 20;
-		double sy = rnd.nextDouble() * 20;
-		double sz = rnd.nextDouble() * 20;
-
-		for (int i = 0; i < 4; i++) {
-			double[][] g = LorenzSystemSolver.solove(i, r, sx + i * 10, sy + i * 10, sz, 0.001, n);
-			testPlot.add(new Plot3D(g[0], g[1], g[2], rnd.nextInt(0xFFFFFF)));
-
-		}
-
-		PlotViewer pw = new PlotViewer(testPlot);
+		final PlotViewer pw = new PlotViewer(testPlot);
 		pw.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		pw.setVisible(true);
+
+		KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+		manager.addKeyEventDispatcher(new KeyEventDispatcher() {
+			double tr = r;
+			int m = testPlot.size() - 1;
+
+			@Override
+			public boolean dispatchKeyEvent(KeyEvent e) {
+				if (e.getID() == KeyEvent.KEY_PRESSED) {
+					if (e.getKeyCode() == 45 || e.getKeyCode() == 61) {
+						if (e.getKeyCode() == 45) {
+							tr = Math.max(1, tr - 1);
+						} else {
+							tr = Math.min(100, tr + 1);
+						}
+
+						for (int i = 0; i < m; i++) {
+							double sx = testPlot.get(i).x[0], sy = testPlot.get(i).y[0], sz = testPlot.get(i).z[0];
+							double[][] g = LorenzSystemSolver.solove(met[i], tr, sx, sy, sz, dt, n);
+							testPlot.get(i).x = g[0];
+							testPlot.get(i).y = g[1];
+							testPlot.get(i).z = g[2];
+						}
+
+						pw.drawPlot();
+						pw.setTitle("[+/-] r = " + tr);
+					}
+				}
+				return false;
+			}
+		});
+
 	}
 }
--- Plot3D.java
+++ Plot3D.java
@@ -6,6 +6,29 @@ public class Plot3D {
 	public int[] color;
 	int size;
 
+	public Plot3D(double[][] p, int rgb) {
+		this.x = p[0];
+		size = x.length;
+		this.y = p[1];
+		size = Math.min(size, y.length);
+		this.z = p[2];
+		size = Math.min(size, z.length);
+
+		this.color = new int[size];
+		Arrays.fill(color, rgb);
+	}
+
+	public Plot3D(double[][] p, int[] color) {
+		this.color = color;
+		size = color.length;
+		this.x = p[0];
+		size = Math.min(size, x.length);
+		this.y = p[1];
+		size = Math.min(size, y.length);
+		this.z = p[2];
+		size = Math.min(size, z.length);
+	}
+
 	public Plot3D(double[] x, double[] y, double[] z, int rgb) {
 		this.x = x;
 		size = x.length;
